b9e1fb80e77dfd3730f7fecfda566a552b9e3ca8
mimarben <mimaben@gmail.com>
2025-06-09 20:26:53 +0200

    uv virtualenv

--- avl/alembic/app/app.py
+++
@@ -1,6 +0,0 @@
-from dotenv import load_dotenv, find_dotenv
-load_dotenv(find_dotenv())
-
-from lib.streamlit_common import *
-
-common_page_initialization("app.py")
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from streamlit-course!")
+
+
+if __name__ == "__main__":
+    main()
