bb33ae9b34e9031c20a11b64c325fc206fff136f
mimarben <mimaben@gmail.com>
2025-09-05 11:31:15 +0200

    Section-12

--- Section-12/main.py
+++ Section-12/main.py
@@ -6,7 +6,7 @@ from st_aggrid import AgGrid, GridOptionsBuilder
 st.set_page_config(page_title="Faker data", layout="wide")
 st.title(":orange[FAKER DATA GENERATOR]")
 st.divider()
-
+df= pd.DataFrame()
 with st.sidebar:
     st.subheader("Configura tu faker Data.")
     localization = st.selectbox("Idioma", options=["en_US","es_MX", "es_ES", "fr_FR", "it_IT","pt_BR"])
@@ -34,10 +34,32 @@ with st.sidebar:
 
 if st.button("Generate Data"):
     data = {field: [faker_options[field]() for _ in range(num_rows)] for field in fields_selection}
+    data["id"]= range(1, num_rows+1)
     df = pd.DataFrame(data)
-    #st.dataframe(df, use_container_width=True, hide_index=True)
+    column_order= ["id"] + [col for col in df.columns if col != "id"]
+    df= df[column_order]    
     gb = GridOptionsBuilder.from_dataframe(df)
     gb.configure_default_column(resizable=True, flex=1)
     gb.configure_pagination(paginationAutoPageSize=False, paginationPageSize=100)
     grid_options = gb.build()
     AgGrid(df, gridOptions=grid_options, height=500, theme="streamlit")
+
+if df.empty == False:
+    col1, col2, _ = st.columns([1,1,8])
+    with col1:        
+        csv= df.to_csv(index=False).encode("utf-8")
+        st.download_button(
+            label="Download CSV",
+            data=csv,
+            file_name="faker_data.csv",
+            mime="text/csv"
+        )
+    with col2:
+        json= df.to_json(orient="records", indent=4, force_ascii=False).encode("utf-8")
+        st.download_button(
+            label="Download json",
+            data=json,
+            file_name="faker_data.json",
+            mime="application/json"
+        )
+    st.divider()
