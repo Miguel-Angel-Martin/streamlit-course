9e4f588afb03b23d42c6b0c01353aab50ffab567
mimarben <mimaben@gmail.com>
2025-09-09 19:59:58 +0200

    scrapper

--- Section-13/scrapper.py
+++ Section-13/scrapper.py
@@ -1,12 +1,17 @@
 import streamlit as st
-from scrapper import scrapper
 import requests
 from bs4 import BeautifulSoup
 from docx import Document
 from io import BytesIO
 
 def scrapper():
-    st.subheader("Wikeipedia web scrapper")
+    st.subheader("Wikipedia web scrapper")
     
-
+    search_term = st.text_input("", placeholder="Search in Wikipedia", autocomplete=False)
+    language = "es"
+    if search_term:
+        url = f"https://{language}.wikipedia.org/wiki/{search_term.replace(' ', '_')}"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        
     
